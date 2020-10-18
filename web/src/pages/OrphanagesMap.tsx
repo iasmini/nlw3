302954d751de497448b9b9b5cf465cc613170ba4
iasmini <iasmini.gomes@gmail.com>
2020-10-18 18:15:26 -0300

    inserido e configurado icone de localizacao no mapa, pag de criacao e detalhes do orfanato e feito link das pags

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,24 @@
+import Leaflet from 'leaflet';
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
 //yarn add @types/react-leaflet -D
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [30, 40],
+  // indica qual posicao do icone deve ser considerado o ponto no mapa (se nao coloca isso, considera o centro
+  // do icone como o ponto no mapa e deve ser a ponta da seta do icone)
+  // 15 eixo x (metade do x do iconsize) e 40 eixo y (ponto mais baixo do iconsize)
+  iconAnchor: [15, 40],
+  popupAnchor: [150, 2]
+})
 
 function OrphanagesMap() {
   return (
@@ -41,9 +53,21 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker
+          icon={mapIcon}
+          position={[-19.9206816,-43.927388]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
       
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF"/>
       </Link>
     </div>
