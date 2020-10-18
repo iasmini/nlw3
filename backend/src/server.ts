422ace8b568fda438b2a441d79d496d603b0294e
iasmini <iasmini.gomes@gmail.com>
2020-10-18 16:14:18 -0300

    criada view para trazer os orfanatos e suas imagens publicadas no localhost

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 // framework em node para lidar com requisicao e resposta
 // yarn add @types/express -D
 import express from 'express';
+import path from 'path';
 
 import './database/connection';
 import routes from "./routes";
@@ -9,5 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
