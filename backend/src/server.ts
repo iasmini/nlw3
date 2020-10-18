96de6f8977b69c5954adf205eb209d4f127f5387
iasmini <iasmini.gomes@gmail.com>
2020-10-18 17:17:12 -0300

    instalado cors e configurado server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
+// yarn add @types/cors -D
+import cors from 'cors';
 // framework em node para lidar com requisicao e resposta
 // yarn add @types/express -D
 import express from 'express';
@@ -10,6 +12,11 @@ import routes from "./routes";
 
 const app = express();
 
+// em producao devemos listar os enderecos que terao acesso
+// app.use(cors({
+//   origin: ['https://...']}
+// ));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
