437917c4b6b862befbda48b987393dd2a7e17553
iasmini <iasmini.gomes@gmail.com>
2020-10-18 14:13:39 -0300

    feito endpoint de listagem de orfanatos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanageController from "./controllers/OrphanageController";
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanageController.index);
 routes.post('/orphanages', OrphanageController.create);
 
 export default routes;
